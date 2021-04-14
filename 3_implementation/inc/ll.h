ab1146333676049dc80c935762027ceead074693
sammy-9930 <80445152+sammy-9930@users.noreply.github.com>
2021-04-14 12:17:56 +0530

    Add files via upload

---
+++ 3_implementation/inc/ll.h
@@ -0,0 +1,9 @@
+#include "student.h"
+
+void insert_at_begin(Student s);
+void insert_at_end(Student s);
+void traverse();
+void delete(int rollNo);
+void deleteNode(struct Node **head_ref, int position);
+void print();
+
