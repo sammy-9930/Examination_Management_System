691181510a7cd83c394c4016a2328cd14790f8ef
root <carol.mendonca@mavenir.com>
2021-04-14 19:11:22 +0530

    Unity

---
+++ 3_implementation/test/test.c
@@ -0,0 +1,34 @@
+#include "ll.h"
+#include "unity.h"
+
+/* Required by the unity test framework */
+void setUp()
+{
+}
+/* Required by the unity test framework */
+void tearDown()
+{
+}
+
+void test_create(void){
+}
+
+void test_file_initilizaion(void){
+
+}
+
+
+
+int main(void)
+{
+    /* Initiate the Unity Test Framework */
+    UNITY_BEGIN();
+
+    /* Run Test functions */
+    //RUN_TEST(check_openFile);
+
+    /* Close the Unity Test Framework */
+    return UNITY_END();
+}
+
+
